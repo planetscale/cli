1244c617926931bb5c77c9bf806e70447a7974c2
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2020-12-02 16:25:43 -0500

    Successfully fetch the access token.

--- pkg/cmd/auth.go
+++ pkg/cmd/auth.go
@@ -1,25 +1,42 @@
 package cmd
 
 import (
+	"bufio"
+	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"os/exec"
 	"runtime"
 	"strings"
+	"time"
 
 	"github.com/cli/safeexec"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
+const (
+	deviceCodeURL = "https://planetscale.us.auth0.com/oauth/device/code"
+	oauthTokenURL = "https://planetscale.us.auth0.com/oauth/token"
+	oauthClientID = "ZK3V2a5UERfOlWxi5xRXrZZFmvhnf1vg"
+)
+
 // DeviceCodeResponse encapsulates the response for obtaining a device code.
 type DeviceCodeResponse struct {
-	DeviceCode      string `json:"device_code"`
-	VerificationURI string `json:"verification_uri"`
-	ExpiresIn       int    `json:"expires_in"`
-	PollingInterval int    `json:"interval"`
+	DeviceCode              string `json:"device_code"`
+	VerificationURI         string `json:"verification_uri"`
+	VerificationCompleteURI string `json:"verification_uri_complete"`
+	ExpiresIn               int    `json:"expires_in"`
+	PollingInterval         int    `json:"interval"`
+}
+
+// ErrorResponse wraps the error from the authorization API.
+type ErrorResponse struct {
+	Error            string `json:"error"`
+	ErrorDescription string `json:"error_description"`
 }
 
 // AuthCmd returns a command for authentication
@@ -34,6 +51,7 @@ func AuthCmd() *cobra.Command {
 	return cmd
 }
 
+// LoginCmd is the command for logging into a PlanetScale account.
 func LoginCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:     "login",
@@ -42,17 +60,19 @@ func LoginCmd() *cobra.Command {
 		Long:    "TODO",
 		Example: "TODO",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := context.Background()
 			fmt.Println("Authenticating")
-			url := "https://planetscale.us.auth0.com/oauth/device/code"
-			payload := strings.NewReader("client_id=ZK3V2a5UERfOlWxi5xRXrZZFmvhnf1vg&scope=profile,email&audience=https://bb-test-api.planetscale.com")
+			payload := strings.NewReader("client_id=ZK3V2a5UERfOlWxi5xRXrZZFmvhnf1vg&scope=profile&audience=https://bb-test-api.planetscale.com")
 
-			req, err := http.NewRequest("POST", url, payload)
+			req, err := http.NewRequest("POST", deviceCodeURL, payload)
 			if err != nil {
 				return err
 			}
 
+			req = req.WithContext(ctx)
 			req.Header.Add("content-type", "application/x-www-form-urlencoded")
 
+			// TODO(iheanyi): Use a better HTTP client than the default one.
 			res, err := http.DefaultClient.Do(req)
 			if err != nil {
 				return err
@@ -62,18 +82,87 @@ func LoginCmd() *cobra.Command {
 			deviceCodeRes := &DeviceCodeResponse{}
 			json.NewDecoder(res.Body).Decode(deviceCodeRes)
 
-			fmt.Println("Please enter the following code in the web browser.")
-			openCmd := OpenBrowserCmd(runtime.GOOS, deviceCodeRes.VerificationURI)
+			fmt.Println("Press Enter to authenticate via your browser...")
+			_ = waitForEnter(cmd.InOrStdin())
+			openCmd := OpenBrowserCmd(runtime.GOOS, deviceCodeRes.VerificationCompleteURI)
 			err = openCmd.Run()
 			if err != nil {
-				errors.Wrap(err, "error opening browser")
+				return errors.Wrap(err, "error opening browser")
 			}
-			fmt.Printf("%v\n", deviceCodeRes)
+
+			// TODO(iheanyi): Revisit why the OAuth login doesn't work as expected.
+			accessToken, err := fetchAccessToken(ctx, deviceCodeRes.DeviceCode, deviceCodeRes.PollingInterval, deviceCodeRes.ExpiresIn)
+			if err != nil {
+				return err
+			}
+
+			fmt.Printf("Authentication complete. Access token: %s\n", accessToken)
 			return nil
 		},
 	}
 
 	return cmd
+}
+
+func fetchAccessToken(ctx context.Context, deviceCode string, pollingInterval int, expiresIn int) (string, error) {
+	expiresAt := time.Now().Add(time.Duration(expiresIn) * time.Second)
+
+	checkInterval := time.Duration(pollingInterval) * time.Second
+	var accessToken string
+	var err error
+
+	for {
+		time.Sleep(checkInterval)
+		accessToken, err = requestToken(ctx, deviceCode)
+		if accessToken == "" && err == nil {
+			if time.Now().After(expiresAt) {
+				err = errors.New("authentication timed out")
+			} else {
+				continue
+			}
+		}
+
+		break
+	}
+
+	return accessToken, err
+}
+
+// OAuthTokenResponse contains the information returned after fetching an access
+// token for a device.
+type OAuthTokenResponse struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+	IDToken      string `json:"id_token"`
+	ExpiresIn    int    `json:"expires_in"`
+}
+
+func requestToken(ctx context.Context, deviceCode string) (string, error) {
+	payload := strings.NewReader(fmt.Sprintf("grant_type=urn%%3Aietf%%3Aparams%%3Aoauth%%3Agrant-type%%3Adevice_code&device_code=%s&client_id=%s", deviceCode, oauthClientID))
+	req, err := http.NewRequest("POST", oauthTokenURL, payload)
+	if err != nil {
+		return "", errors.Wrap(err, "error creating request")
+	}
+
+	req.Header.Add("content-type", "application/x-www-form-urlencoded")
+
+	req.WithContext(ctx)
+
+	// TODO(iheanyi): Use a better HTTP client than the default one.
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", errors.Wrap(err, "error performing http request")
+	}
+
+	defer res.Body.Close()
+	tokenRes := &OAuthTokenResponse{}
+
+	err = json.NewDecoder(res.Body).Decode(tokenRes)
+	if err != nil {
+		return "", errors.Wrap(err, "error decoding token response")
+	}
+
+	return tokenRes.AccessToken, nil
 }
 
 func linuxExe() string {
@@ -105,10 +194,15 @@ func OpenBrowserCmd(goos, url string) *exec.Cmd {
 		exe = linuxExe()
 		args = append(args, url)
 	}
-	fmt.Println(exe)
 	cmd := exec.Command(exe, args...)
 	cmd.Stderr = os.Stderr
 	return cmd
+}
+
+func waitForEnter(r io.Reader) error {
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+	return scanner.Err()
 }
 
 var lookPath = safeexec.LookPath
