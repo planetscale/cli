533a9a69ce45e312dcea1e80ef1af0398798d49a
Iheanyi Ekechukwu <iheanyi@planetscale.com>
2020-12-15 13:09:40 -0500

    Remove DatabaseStatus from Database struct.

--- psapi/databases.go
+++ psapi/databases.go
@@ -28,13 +28,12 @@ type DatabasesService interface {
 
 // Database represents a PlanetScale database
 type Database struct {
-	ID          int64           `json:"id,omitempty" header:"id"`
-	Label       string          `json:"label" header:"label"`
-	Slug        string          `json:"slug" header:"slug"`
-	Description string          `json:"description" header:"description"`
-	CreatedAt   *time.Time      `json:"created_at" header:"created_at,unixtime_human"`
-	UpdatedAt   *time.Time      `json:"updated_at" header:"updated_at,unixtime_human"`
-	Status      *DatabaseStatus `json:"status"`
+	ID          int64      `json:"id,omitempty" header:"id"`
+	Label       string     `json:"label" header:"label"`
+	Slug        string     `json:"slug" header:"slug"`
+	Description string     `json:"description" header:"description"`
+	CreatedAt   *time.Time `json:"created_at" header:"created_at,unixtime_human"`
+	UpdatedAt   *time.Time `json:"updated_at" header:"updated_at,unixtime_human"`
 }
 
 // DatabaseStatus represents the status of a PlanetScale database.
