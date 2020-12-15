88fa8db6542f6491585df93df353956c24a8995c
notfelineit <notfelineit@gmail.com>
2020-12-15 13:22:44 -0800

    Update url encoding

--- pkg/cmd/database/create.go
+++ pkg/cmd/database/create.go
@@ -3,7 +3,9 @@ package database
 import (
 	"context"
 	"fmt"
+	"net/url"
 
+	"github.com/pkg/browser"
 	"github.com/planetscale/cli/config"
 	"github.com/planetscale/cli/psapi"
 	"github.com/spf13/cobra"
@@ -18,6 +20,20 @@ func CreateCmd(cfg *config.Config) *cobra.Command {
 		Use: "create",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			ctx := context.Background()
+			web, err := cmd.Flags().GetBool("web")
+			if err != nil {
+				return err
+			}
+
+			if web {
+				fmt.Println("🌐  Redirecting you to create a database in your web browser.")
+				err := browser.OpenURL(fmt.Sprintf("https://app.planetscaledb.io/databases?slug=%s&label=%s&description=%s&showDialog=true", url.QueryEscape(createReq.Database.Slug), url.QueryEscape(createReq.Database.Label), url.QueryEscape(createReq.Database.Description)))
+				if err != nil {
+					return err
+				}
+				return nil
+			}
+
 			client, err := cfg.NewClientFromConfig()
 			if err != nil {
 				return err
@@ -39,6 +55,7 @@ func CreateCmd(cfg *config.Config) *cobra.Command {
 	cmd.Flags().StringVarP(&createReq.Database.Description, "description", "d", "", "a description for the database (required)")
 	_ = cmd.MarkFlagRequired("label")
 	_ = cmd.MarkFlagRequired("slug")
+	cmd.Flags().BoolP("web", "w", false, "Create a database in your web browser")
 
 	return cmd
 }
