fa8c3498ffc5a35743270cf30aac87743f327753
Iheanyi Ekechukwu <iheanyi@planetscale.com>
2020-12-22 12:55:52 -0500

    Pass api-token in as a flag.

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -69,6 +69,7 @@ func init() {
 	cfg := config.New()
 
 	rootCmd.PersistentFlags().StringVar(&cfg.BaseURL, "api-url", psapi.DefaultBaseURL, "The base URL for the PlanetScale API.")
+	rootCmd.PersistentFlags().StringVar(&cfg.AccessToken, "api-token", cfg.AccessToken, "The API token for a given user")
 	rootCmd.AddCommand(auth.AuthCmd(cfg))
 	rootCmd.AddCommand(DebugCmd(cfg))
 	rootCmd.AddCommand(database.DatabaseCmd(cfg))
