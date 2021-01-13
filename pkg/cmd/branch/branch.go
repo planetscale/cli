cedd0a062b6b233aac15787bd219fdd4b047105c
Iheanyi Ekechukwu <iheanyi@planetscale.com>
2021-01-13 15:44:24 -0500

    Get started on scaffolding for the branch command.

--- pkg/cmd/branch/branch.go
+++ pkg/cmd/branch/branch.go
@@ -1,18 +1,31 @@
 package branch
 
 import (
+	"fmt"
+
 	"github.com/planetscale/cli/config"
 	"github.com/spf13/cobra"
 )
 
 func BranchCmd(cfg *config.Config) *cobra.Command {
+	var notes string
 	cmd := &cobra.Command{
-		Use:     "branch <command|source-database> <branch-name> [options]",
+		Use:     "branch <source-database> <branch-name> [options]",
 		Short:   "Branch a production database",
 		Aliases: []string{"b"},
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			if len(args) != 2 {
+				return cmd.Usage()
+			}
+
+			fmt.Println("database branching has not been implemented yet")
+			return nil
+		},
 	}
 
-	cmd.Flags().StringVar(nil, "notes", "", "notes for the database branch")
+	cmd.Flags().StringVar(&notes, "notes", "", "notes for the database branch")
+	cmd.AddCommand(ListCmd(cfg))
 
 	return cmd
 }
