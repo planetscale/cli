2b67aeb5ee444171a598b6fc8d52dd522ff0ab45
Iheanyi Ekechukwu <iheanyi@planetscale.com>
2021-01-13 15:45:38 -0500

    Add more scaffolding.

--- pkg/cmd/branch/branch.go
+++ pkg/cmd/branch/branch.go
@@ -14,10 +14,18 @@ func BranchCmd(cfg *config.Config) *cobra.Command {
 		Short:   "Branch a production database",
 		Aliases: []string{"b"},
 		RunE: func(cmd *cobra.Command, args []string) error {
-
+			// If the user does not provide a source database and a branch name,
+			// show the usage.
 			if len(args) != 2 {
 				return cmd.Usage()
 			}
+
+			_, err := cfg.NewClientFromConfig()
+			if err != nil {
+				return err
+			}
+
+			// TODO: Call PlanetScale API client here to create the database branch.
 
 			fmt.Println("database branching has not been implemented yet")
 			return nil
