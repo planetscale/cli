62ce44662690c82a1c63ef440d225b411a792d50
Iheanyi Ekechukwu <iheanyi@planetscale.com>
2021-01-14 16:37:27 -0500

    Add NewDatabaseBranchSlicePrinter.

--- printer/database_branch.go
+++ printer/database_branch.go
@@ -13,7 +13,7 @@ type DatabaseBranch struct {
 	UpdatedAt int64  `header:"updated_at,timestamp(ms|utc|human)"`
 }
 
-// DatabaseBranchPrinter returns a struct that prints out the various fields of a
+// NewDatabaseBranchPrinter returns a struct that prints out the various fields of a
 // database model.
 func NewDatabaseBranchPrinter(db *ps.DatabaseBranch) *DatabaseBranch {
 	return &DatabaseBranch{
@@ -22,4 +22,19 @@ func NewDatabaseBranchPrinter(db *ps.DatabaseBranch) *DatabaseBranch {
 		CreatedAt: db.CreatedAt.UTC().UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond)),
 		UpdatedAt: db.UpdatedAt.UTC().UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond)),
 	}
+}
+
+func NewDatabaseBranchSlicePrinter(branches []*ps.DatabaseBranch) []*DatabaseBranch {
+	bs := make([]*DatabaseBranch, 0, len(branches))
+
+	for _, db := range branches {
+		bs = append(bs, &DatabaseBranch{
+			Name:      db.Name,
+			Notes:     db.Notes,
+			CreatedAt: db.CreatedAt.UTC().UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond)),
+			UpdatedAt: db.UpdatedAt.UTC().UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond)),
+		})
+	}
+
+	return bs
 }
