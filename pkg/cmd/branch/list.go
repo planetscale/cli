5af8fd8846bf0f05aac9f3ae6a0eb72b9fb926ee
Iheanyi Ekechukwu <iheanyi@planetscale.com>
2021-01-14 16:34:16 -0500

    Implement actual business logic for branch listing command.

--- pkg/cmd/branch/list.go
+++ pkg/cmd/branch/list.go
@@ -4,8 +4,12 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"os"
 
+	"github.com/lensesio/tableprinter"
+	errs "github.com/pkg/errors"
 	"github.com/planetscale/cli/config"
+	"github.com/planetscale/cli/printer"
 	"github.com/spf13/cobra"
 )
 
@@ -15,8 +19,8 @@ func ListCmd(cfg *config.Config) *cobra.Command {
 		Use:   "list <db-name>",
 		Short: "List all branches of a database",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			_ = context.Background()
-			_, err := cfg.NewClientFromConfig()
+			ctx := context.Background()
+			client, err := cfg.NewClientFromConfig()
 			if err != nil {
 				return err
 			}
@@ -25,11 +29,22 @@ func ListCmd(cfg *config.Config) *cobra.Command {
 				return errors.New("<db_name> is missing")
 			}
 
-			_ = args[0]
+			source := args[0]
 
 			// TODO: Actually call database branch endpoints here.
+			branches, err := client.DatabaseBranches.List(ctx, cfg.Organization, source)
+			if err != nil {
+				return errs.Wrap(err, "error listing databases")
+			}
 
-			fmt.Println("branch listing not implemented yet")
+			// Technically, this should never actually happen.
+			if len(branches) == 0 {
+				fmt.Println("No branches exist for this database.")
+				return nil
+			}
+
+			tableprinter.Print(os.Stdout, printer.NewDatabaseBranchSlicePrinter(branches))
+
 			return nil
 		},
 	}
