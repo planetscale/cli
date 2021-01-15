cbd242612488d1bfed2ef1dc40e22ccf426572b6
Iheanyi Ekechukwu <iheanyi@planetscale.com>
2021-01-15 18:22:59 -0500

    Add ParentBranch to output.

--- printer/database_branch.go
+++ printer/database_branch.go
@@ -7,22 +7,24 @@ import (
 )
 
 type DatabaseBranch struct {
-	Name      string `header:"name"`
-	Notes     string `header:"notes"`
-	CreatedAt int64  `header:"created_at,timestamp(ms|utc|human)"`
-	UpdatedAt int64  `header:"updated_at,timestamp(ms|utc|human)"`
-	Status    string `header:"status"`
+	Name         string `header:"name"`
+	Notes        string `header:"notes"`
+	Status       string `header:"status"`
+	ParentBranch string `header:"parent branch,n/a"`
+	CreatedAt    int64  `header:"created_at,timestamp(ms|utc|human)"`
+	UpdatedAt    int64  `header:"updated_at,timestamp(ms|utc|human)"`
 }
 
 // NewDatabaseBranchPrinter returns a struct that prints out the various fields of a
 // database model.
 func NewDatabaseBranchPrinter(db *ps.DatabaseBranch) *DatabaseBranch {
 	return &DatabaseBranch{
-		Name:      db.Name,
-		Notes:     db.Notes,
-		Status:    db.Status,
-		CreatedAt: db.CreatedAt.UTC().UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond)),
-		UpdatedAt: db.UpdatedAt.UTC().UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond)),
+		Name:         db.Name,
+		Notes:        db.Notes,
+		Status:       db.Status,
+		ParentBranch: db.ParentBranch,
+		CreatedAt:    db.CreatedAt.UTC().UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond)),
+		UpdatedAt:    db.UpdatedAt.UTC().UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond)),
 	}
 }
 
