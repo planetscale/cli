1555422b2e5c3d298ebb671fff8599589dbe2250
Iheanyi Ekechukwu <iheanyi@planetscale.com>
2021-01-29 16:50:48 -0500

    Flesh out snapshot get command.

--- pkg/cmd/snapshot/get.go
+++ pkg/cmd/snapshot/get.go
@@ -1,7 +1,13 @@
 package snapshot
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/planetscale/cli/cmdutil"
 	"github.com/planetscale/cli/config"
+	"github.com/planetscale/cli/printer"
+	"github.com/planetscale/planetscale-go/planetscale"
 	"github.com/spf13/cobra"
 )
 
@@ -11,6 +17,39 @@ func GetCmd(cfg *config.Config) *cobra.Command {
 		Use:   "get <snapshot_id>",
 		Short: "Get a specific schema snapshot",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := context.Background()
+
+			client, err := cfg.NewClientFromConfig()
+			if err != nil {
+				return err
+			}
+
+			if len(args) != 1 {
+				return cmd.Usage()
+			}
+
+			id := args[0]
+			end := cmdutil.PrintProgress(fmt.Sprintf("Fetching schema snapshot %s", cmdutil.BoldBlue(id)))
+			defer end()
+
+			snapshot, err := client.SchemaSnapshots.Get(ctx, &planetscale.GetSchemaSnapshotRequest{
+				ID: id,
+			})
+			if err != nil {
+				return err
+			}
+			end()
+
+			isJSON, err := cmd.Flags().GetBool("json")
+			if err != nil {
+				return err
+			}
+
+			err = printer.PrintOutput(isJSON, printer.NewSchemaSnapshotPrinter(snapshot))
+			if err != nil {
+				return err
+			}
+
 			return nil
 		},
 	}
