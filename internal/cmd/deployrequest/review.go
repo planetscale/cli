5ee6dbddc14565b34dbf9a1d1f1ebca9b34cd296
Fatih Arslan <ftharsln@gmail.com>
2021-03-31 18:00:31 +0300

    Add dr review

--- internal/cmd/deployrequest/review.go
+++ internal/cmd/deployrequest/review.go
@@ -1,10 +1,14 @@
 package deployrequest
 
 import (
+	"context"
 	"errors"
+	"fmt"
+	"strconv"
 
 	"github.com/planetscale/cli/internal/cmdutil"
 	"github.com/planetscale/cli/internal/config"
+	"github.com/planetscale/planetscale-go/planetscale"
 
 	"github.com/spf13/cobra"
 )
@@ -26,12 +30,46 @@ func ReviewCmd(cfg *config.Config) *cobra.Command {
 				return errors.New("neither --approve nor --comment is set")
 			}
 
-			_, err := cfg.NewClientFromConfig()
+			ctx := context.Background()
+			database := args[0]
+			number := args[1]
+
+			n, err := strconv.ParseUint(number, 10, 64)
+			if err != nil {
+				return fmt.Errorf("The argument <number> is invalid: %s", err)
+			}
+
+			client, err := cfg.NewClientFromConfig()
 			if err != nil {
 				return err
 			}
 
-			return errors.New("not implemented yet")
+			state := "commented"
+			if flags.approve {
+				state = "approved"
+			}
+
+			_, err = client.DeployRequests.CreateReview(ctx, &planetscale.ReviewDeployRequestRequest{
+				Organization: cfg.Organization,
+				Database:     database,
+				Number:       n,
+				State:        state,
+				Body:         flags.comment,
+			})
+			if err != nil {
+				return err
+			}
+
+			switch state {
+			case "approved":
+				fmt.Printf("Deploy request %s/%s is approved.\n",
+					cmdutil.BoldBlue(database), cmdutil.BoldBlue(number))
+			case "commented":
+				fmt.Printf("A comment is added to the deploy request %s/%s.\n",
+					cmdutil.BoldBlue(database), cmdutil.BoldBlue(number))
+			}
+
+			return nil
 		},
 	}
 
