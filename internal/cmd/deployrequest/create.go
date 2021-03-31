1c24e5dca13232594a758e76335c7d00086ea969
Fatih Arslan <ftharsln@gmail.com>
2021-03-31 19:23:17 +0300

    Add dr create

--- internal/cmd/deployrequest/create.go
+++ internal/cmd/deployrequest/create.go
@@ -1,29 +1,68 @@
 package deployrequest
 
 import (
-	"errors"
+	"context"
+	"fmt"
 
 	"github.com/planetscale/cli/internal/cmdutil"
 	"github.com/planetscale/cli/internal/config"
+	"github.com/planetscale/cli/internal/printer"
+	"github.com/planetscale/planetscale-go/planetscale"
 
 	"github.com/spf13/cobra"
 )
 
 // CreateCmd is the command for creating deploy requests.
 func CreateCmd(cfg *config.Config) *cobra.Command {
+	var flags struct {
+		deployTo string
+	}
+
 	cmd := &cobra.Command{
-		Use:   "create <database> <branch>",
+		Use:   "create <database> <branch> [flags]",
 		Short: "Create a deploy request from a branch",
 		Args:  cmdutil.RequiredArgs("database", "branch"),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			_, err := cfg.NewClientFromConfig()
+			ctx := context.Background()
+			database := args[0]
+			branch := args[1]
+
+			client, err := cfg.NewClientFromConfig()
 			if err != nil {
 				return err
 			}
 
-			return errors.New("not implemented yet")
+			end := cmdutil.PrintProgress(fmt.Sprintf("Request deploying of %s branch in %s...", cmdutil.BoldBlue(branch), cmdutil.BoldBlue(database)))
+			defer end()
+
+			dr, err := client.DeployRequests.Create(ctx, &planetscale.CreateDeployRequestRequest{
+				Organization: cfg.Organization,
+				Database:     database,
+				Branch:       branch,
+				IntoBranch:   flags.deployTo,
+			})
+			if err != nil {
+				return err
+			}
+			end()
+
+			if cfg.OutputJSON {
+				err := printer.PrintJSON(dr)
+				if err != nil {
+					return err
+				}
+			} else {
+				fmt.Printf("Successfully requested deploy %s of %s into %s!\n",
+					cmdutil.BoldBlue(dr.ID),
+					cmdutil.BoldBlue(dr.Branch),
+					cmdutil.BoldBlue(dr.IntoBranch))
+			}
+
+			return nil
 		},
 	}
+
+	cmd.PersistentFlags().StringVar(&flags.deployTo, "deploy-to", "main", "Branch to deploy the branch. By default it's set to 'main'")
 
 	return cmd
 }
