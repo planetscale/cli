d5104652876e4489b966b3791e9f1a8c1a23e4ed
Fatih Arslan <ftharsln@gmail.com>
2021-03-31 16:54:53 +0300

    Add deploy-request show

--- internal/cmd/deployrequest/show.go
+++ internal/cmd/deployrequest/show.go
@@ -1,11 +1,15 @@
 package deployrequest
 
 import (
-	"errors"
+	"context"
 	"fmt"
+	"strconv"
 
+	"github.com/pkg/browser"
 	"github.com/planetscale/cli/internal/cmdutil"
 	"github.com/planetscale/cli/internal/config"
+	"github.com/planetscale/cli/internal/printer"
+	"github.com/planetscale/planetscale-go/planetscale"
 
 	"github.com/spf13/cobra"
 )
@@ -21,18 +25,40 @@ func ShowCmd(cfg *config.Config) *cobra.Command {
 		Short: "Show a specific deploy request",
 		Args:  cmdutil.RequiredArgs("database", "number"),
 		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := context.Background()
+			database := args[0]
+			number := args[1]
+
 			if flags.web {
 				fmt.Println("🌐  Redirecting you to your deploy request in your web browser.")
-				// TODO(fatih): immplement
-				return nil
+				return browser.OpenURL(fmt.Sprintf("%s/%s/%s/deploy-requests/%s", cmdutil.ApplicationURL, cfg.Organization, database, number))
 			}
 
-			_, err := cfg.NewClientFromConfig()
+			client, err := cfg.NewClientFromConfig()
 			if err != nil {
 				return err
 			}
 
-			return errors.New("not implemented yet")
+			n, err := strconv.ParseUint(number, 10, 64)
+			if err != nil {
+				return fmt.Errorf("The argument <number> is invalid: %s", err)
+			}
+
+			dr, err := client.DeployRequests.Get(ctx, &planetscale.GetDeployRequestRequest{
+				Organization: cfg.Organization,
+				Database:     database,
+				Number:       n,
+			})
+			if err != nil {
+				return err
+			}
+
+			err = printer.PrintOutput(cfg.OutputJSON, printer.NewDeployRequestPrinter(dr))
+			if err != nil {
+				return err
+			}
+
+			return nil
 		},
 	}
 
