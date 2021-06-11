d048b054cdaaf769f70514b242a1ad813abfde93
Iheanyi Ekechukwu <iheanyi@planetscale.com>
2021-06-11 13:59:01 -0500

    Add regions to database creation.

--- internal/cmd/database/create.go
+++ internal/cmd/database/create.go
@@ -8,6 +8,7 @@ import (
 	"github.com/planetscale/cli/internal/cmdutil"
 	"github.com/planetscale/cli/internal/printer"
 
+	"github.com/planetscale/planetscale-go/planetscale"
 	ps "github.com/planetscale/planetscale-go/planetscale"
 
 	"github.com/pkg/browser"
@@ -70,6 +71,29 @@ func CreateCmd(ch *cmdutil.Helper) *cobra.Command {
 	}
 
 	cmd.Flags().StringVar(&createReq.Notes, "notes", "", "notes for the database")
+	cmd.Flags().StringVar(&createReq.Region, "region", "", "region for the database")
+	cmd.RegisterFlagCompletionFunc("region", func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		client, err := ch.Client()
+		if err != nil {
+			return nil, cobra.ShellCompDirectiveNoFileComp
+		}
+
+		regions, err := client.Regions.List(context.Background(), &planetscale.ListRegionsRequest{})
+		if err != nil {
+			return nil, cobra.ShellCompDirectiveNoFileComp
+		}
+
+		regionStrs := make([]string, 0)
+
+		for _, r := range regions {
+			if r.Enabled {
+				regionStrs = append(regionStrs, r.Slug)
+			}
+		}
+
+		return regionStrs, cobra.ShellCompDirectiveDefault
+	})
+
 	cmd.Flags().BoolP("web", "w", false, "Create a database in your web browser")
 
 	return cmd
