c9a5129eb22d0adfd49618ca1c4d428283519e73
Fatih Arslan <ftharsln@gmail.com>
2021-07-09 19:10:02 +0300

    We don't need to define the registry

--- script/release.sh
+++ script/release.sh
@@ -34,7 +34,7 @@ cd /tmp && tar -zxvf goreleaser_Linux_x86_64.tar.gz
 echo "--- running goreleaser"
 
 echo "Login to the docker..."
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin "$DOCKER_REGISTRY"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 export GORELEASER_CURRENT_TAG=$(buildkite-agent meta-data get "release-version")
 
