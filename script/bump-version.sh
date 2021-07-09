4c457a42a30d67e05a597d930b6e74d4856e27c2
Fatih Arslan <ftharsln@gmail.com>
2021-07-09 19:12:48 +0300

    Specify the version

--- script/bump-version.sh
+++ script/bump-version.sh
@@ -8,7 +8,8 @@ SVU_BIN="${WORKDIR}/svu"
 echo "+++ :construction:  Installing 'svu' tool"
 curl -sfL https://install.goreleaser.com/github.com/caarlos0/svu.sh | bash -s -- -b $WORKDIR
 
-RELEASE_VERSION=$($SVU_BIN minor)
+# RELEASE_VERSION=$($SVU_BIN minor)
+RELEASE_VERSION="v0.56.0-rc1"
 
 echo "+++ :boom: Bumping to version $RELEASE_VERSION"
 
@@ -16,7 +17,7 @@ git config --global --add url."https://${GITHUB_TOKEN}@github.com/".insteadOf "h
 
 buildkite-agent meta-data set "release-version" "$RELEASE_VERSION"
 
-# git tag "$RELEASE_VERSION"
-# git push origin "$RELEASE_VERSION"
+git tag "$RELEASE_VERSION"
+git push origin "$RELEASE_VERSION"
 
 echo "✅"
