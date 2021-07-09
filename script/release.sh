6544078bbdec785730c9864767865eee668a32dd
Fatih Arslan <ftharsln@gmail.com>
2021-07-09 17:40:38 +0300

    ci: install dependencies required for `goreleaser`

--- script/release.sh
+++ script/release.sh
@@ -4,6 +4,27 @@ set -eu
 
 WORKDIR=$(pwd)
 
+
+export DEBIAN_FRONTEND=noninteractive
+
+echo "--- installing docker cli"
+apt-get update
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install -y docker-ce-cli 
+
 echo "--- installing goreleaser"
 
 curl -L -o /tmp/goreleaser_Linux_x86_64.tar.gz https://github.com/goreleaser/goreleaser/releases/download/v0.173.2/goreleaser_Linux_x86_64.tar.gz
