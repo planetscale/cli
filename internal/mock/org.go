05d4826089c837c21e2f7e293ea6770d35416553
Iheanyi Ekechukwu <iheanyi@planetscale.com>
2021-07-30 17:16:50 -0500

    Add tests.

--- internal/mock/org.go
+++ internal/mock/org.go
@@ -12,6 +12,9 @@ type OrganizationsService struct {
 
 	ListFn        func(context.Context) ([]*ps.Organization, error)
 	ListFnInvoked bool
+
+	ListRegionsFn        func(context.Context, *ps.ListOrganizationRegionsRequest) ([]*ps.Region, error)
+	ListRegionsFnInvoked bool
 }
 
 func (o *OrganizationsService) Get(ctx context.Context, req *ps.GetOrganizationRequest) (*ps.Organization, error) {
@@ -22,4 +25,9 @@ func (o *OrganizationsService) Get(ctx context.Context, req *ps.GetOrganizationR
 func (o *OrganizationsService) List(ctx context.Context) ([]*ps.Organization, error) {
 	o.ListFnInvoked = true
 	return o.ListFn(ctx)
+}
+
+func (o *OrganizationsService) ListRegions(ctx context.Context, req *ps.ListOrganizationRegionsRequest) ([]*ps.Region, error) {
+	o.ListRegionsFnInvoked = true
+	return o.ListRegionsFn(ctx, req)
 }
