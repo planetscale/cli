fe3b97f5166808dcb3f57d8d616fd3338ad1fb2c
Fatih Arslan <ftharsln@gmail.com>
2021-08-03 21:46:20 +0300

    ci: make sure BuildKite have access to latest tags

--- script/bump-version.sh
+++ script/bump-version.sh
@@ -8,6 +8,8 @@ SVU_BIN="${WORKDIR}/svu"
 echo "+++ :construction:  Installing 'svu' tool"
 curl -sfL https://install.goreleaser.com/github.com/caarlos0/svu.sh | bash -s -- -b $WORKDIR
 
+git fetch --tags 
+
 RELEASE_VERSION=$($SVU_BIN minor)
 
 echo "+++ :boom: Bumping to version $RELEASE_VERSION"
