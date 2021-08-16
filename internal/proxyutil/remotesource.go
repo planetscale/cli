943dd43e5b4d05dfac1311e2976d26c864270e7b
Dirkjan Bussink <d.bussink@gmail.com>
2021-08-16 18:07:55 +0200

    Update dependencies with cleaned up validation logic
    This updates all the dependencies of the CLI with the cleaned up logic
    around TLS validation.

--- internal/proxyutil/remotesource.go
+++ internal/proxyutil/remotesource.go
@@ -39,8 +39,7 @@ func (r *RemoteCertSource) Cert(ctx context.Context, org, db, branch string) (*p
 
 	return &proxy.Cert{
 		ClientCert: cert.ClientCert,
-		CACerts:    cert.CACerts,
-		RemoteAddr: cert.RemoteAddr,
+		AccessHost: cert.AccessHost,
 		Ports: proxy.RemotePorts{
 			Proxy: cert.Ports.Proxy,
 			MySQL: cert.Ports.MySQL,
