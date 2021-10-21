9610042e24ea90da087736bda1e4787c953a5065
Johannes Nicolai <jonico@planetscale.com>
2021-10-21 19:44:24 +0200

    Update internal/cmd/branch/delete.go
    Co-authored-by: Nick Van Wiggeren <nickvanw@users.noreply.github.com>

--- internal/cmd/branch/delete.go
+++ internal/cmd/branch/delete.go
@@ -75,7 +75,7 @@ func DeleteCmd(ch *cmdutil.Helper) *cobra.Command {
 			if err != nil {
 				switch cmdutil.ErrCode(err) {
 				case planetscale.ErrNotFound:
-					return fmt.Errorf("database %s and branch %s does not exist in organization %s",
+					return fmt.Errorf("database %s or branch %s does not exist in organization %s",
 						printer.BoldBlue(source), printer.BoldBlue(branch), printer.BoldBlue(ch.Config.Organization))
 				default:
 					return cmdutil.HandleError(err)
