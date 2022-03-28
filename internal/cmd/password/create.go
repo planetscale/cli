d4a3dc991a0a02d18e7c74158ae70c94d98c4f5e
Nick Van Wiggeren <nick@planetscale.com>
2022-03-28 13:52:28 -0700

    make default admin

--- internal/cmd/password/create.go
+++ internal/cmd/password/create.go
@@ -70,7 +70,7 @@ func CreateCmd(ch *cmdutil.Helper) *cobra.Command {
 		},
 	}
 	cmd.PersistentFlags().StringVar(&flags.role, "role",
-		"reader", "Role defines the access level, allowed values are : reader, writer, readwriter, admin. By default it is reader.")
+		"admin", "Role defines the access level, allowed values are : reader, writer, readwriter, admin. By default it is admin.")
 	cmd.PersistentFlags().MarkHidden("role")
 	return cmd
 }
