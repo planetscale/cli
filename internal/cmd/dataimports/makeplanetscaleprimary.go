bd0ae1b2a849ee6d2b2ee2b01c6cd72647821e10
Phani Raj <phani@planetscale.com>
2022-05-10 13:29:25 -0500

    support make-primary and make-replica

--- internal/cmd/dataimports/makeplanetscaleprimary.go
+++ internal/cmd/dataimports/makeplanetscaleprimary.go
@@ -1,25 +1,18 @@
 package dataimports
 
 import (
-	"errors"
 	"fmt"
 	"github.com/planetscale/cli/internal/cmdutil"
-	"github.com/planetscale/cli/internal/printer"
 	ps "github.com/planetscale/planetscale-go/planetscale"
 	"github.com/spf13/cobra"
-	"strings"
 )
 
 func MakePlanetScalePrimaryCmd(ch *cmdutil.Helper) *cobra.Command {
 	var flags struct {
-		host     string
-		username string
-		password string
-		database string
-		port     int
+		name string
 	}
 
-	testRequest := &ps.TestDataImportSourceRequest{}
+	makePrimaryReq := &ps.MakePlanetScalePrimaryRequest{}
 
 	cmd := &cobra.Command{
 		Use:     "make-primary [options]",
@@ -28,57 +21,29 @@ func MakePlanetScalePrimaryCmd(ch *cmdutil.Helper) *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			ctx := cmd.Context()
 
-			testRequest.Organization = ch.Config.Organization
-			testRequest.Source = ps.DataImportSource{
-				Database: flags.database,
-				UserName: flags.username,
-				Password: flags.password,
-				HostName: flags.host,
-				Port:     flags.port,
-			}
+			makePrimaryReq.Organization = ch.Config.Organization
+			makePrimaryReq.DatabaseName = flags.name
 
 			client, err := ch.Client()
 			if err != nil {
 				return err
 			}
 
-			end := ch.Printer.PrintProgress(fmt.Sprintf("Testing Compatibility of database %s with user %s...", printer.BoldBlue(flags.database), printer.BoldBlue(flags.username)))
-			defer end()
-
-			resp, err := client.DataImports.TestDataImportSource(ctx, testRequest)
+			_, err = client.DataImports.MakePlanetScalePrimary(ctx, makePrimaryReq)
 			if err != nil {
 				switch cmdutil.ErrCode(err) {
 				case ps.ErrNotFound:
-					return fmt.Errorf("unable to check compatibility of database %s, hosted at %s", flags.database, flags.host)
+					return fmt.Errorf("unable to switch PlanetScale database %s to primary", flags.name)
 				default:
 					return cmdutil.HandleError(err)
 				}
 			}
 
-			if len(resp.Errors) > 0 {
-
-				var sb strings.Builder
-				sb.WriteString(printer.Red("Branch promotion failed. "))
-				sb.WriteString("Fix the following errors and then try again:\n\n")
-				for _, compatError := range resp.Errors {
-					fmt.Fprintf(&sb, "• %s\n", compatError.ErrorDescription)
-				}
-
-				return errors.New(sb.String())
-			}
-
-			end()
-
-			ch.Printer.Printf("Database %s hosted at %s is compatible and can be imported into PlanetScale ", flags.database, flags.host)
 			return nil
 		},
 	}
 
-	cmd.PersistentFlags().StringVar(&flags.host, "host", "", "")
-	cmd.PersistentFlags().StringVar(&flags.database, "database", "", "")
-	cmd.PersistentFlags().StringVar(&flags.username, "username", "", "")
-	cmd.PersistentFlags().StringVar(&flags.password, "password", "", "")
-	cmd.PersistentFlags().IntVar(&flags.port, "port", 3306, "")
+	cmd.PersistentFlags().StringVar(&flags.name, "name", "", "")
 
 	return cmd
 }
