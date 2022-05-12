c0da58930916e474ac882e3550b708e4776d5c25
Phani Raj <phani@planetscale.com>
2022-05-12 10:38:10 -0500

    confirm cancellation of import

--- internal/cmd/dataimports/cancel.go
+++ internal/cmd/dataimports/cancel.go
@@ -1,14 +1,21 @@
 package dataimports
 
 import (
+	"errors"
+	"fmt"
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/AlecAivazis/survey/v2/terminal"
 	"github.com/planetscale/cli/internal/cmdutil"
+	"github.com/planetscale/cli/internal/printer"
 	ps "github.com/planetscale/planetscale-go/planetscale"
 	"github.com/spf13/cobra"
+	"os"
 )
 
 func CancelDataImportCmd(ch *cmdutil.Helper) *cobra.Command {
 	var flags struct {
-		name string
+		name  string
+		force bool
 	}
 
 	cancelRequest := &ps.CancelDataImportRequest{}
@@ -20,6 +27,10 @@ func CancelDataImportCmd(ch *cmdutil.Helper) *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			ctx := cmd.Context()
 
+			if flags.name == "" {
+				flags.name = args[0]
+			}
+
 			cancelRequest.Organization = ch.Config.Organization
 			cancelRequest.Database = flags.name
 
@@ -28,17 +39,50 @@ func CancelDataImportCmd(ch *cmdutil.Helper) *cobra.Command {
 				return err
 			}
 
-			dataImport, err := client.DataImports.CancelDataImport(ctx, cancelRequest)
+			if !flags.force {
+				if ch.Printer.Format() != printer.Human {
+					return fmt.Errorf("cannot cancel import with the output format %q (run with -force to override)", ch.Printer.Format())
+				}
+
+				confirmationName := fmt.Sprintf("%s/%s", cancelRequest.Organization, cancelRequest.Database)
+				if !printer.IsTTY {
+					return fmt.Errorf("cannot confirm cancellation of import %q (run with -force to override)", confirmationName)
+				}
+
+				confirmationMessage := fmt.Sprintf("%s %s %s", printer.Bold("Please type"), printer.BoldBlue(confirmationName), printer.Bold("to confirm:"))
+
+				prompt := &survey.Input{
+					Message: confirmationMessage,
+				}
+
+				var userInput string
+				err := survey.AskOne(prompt, &userInput)
+				if err != nil {
+					if err == terminal.InterruptErr {
+						os.Exit(0)
+					} else {
+						return err
+					}
+				}
+
+				// If the confirmations don't match up, let's return an error.
+				if userInput != confirmationName {
+					return errors.New("incorrect import name entered, skipping import cancellation")
+				}
+			}
+
+			err = client.DataImports.CancelDataImport(ctx, cancelRequest)
 			if err != nil {
 				return err
 			}
 
-			ch.Printer.PrintDataImport(*dataImport)
+			ch.Printer.Printf("Data import into database %v has been canceleed", flags.name)
 			return nil
 		},
 	}
 
 	cmd.PersistentFlags().StringVar(&flags.name, "name", "", "")
+	cmd.Flags().BoolVar(&flags.force, "force", false, "Cancel an import without confirmation")
 
 	return cmd
 }
