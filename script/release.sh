bc1871fcecf490be2e3d65884a196a4daece00a5
Dirkjan Bussink <d.bussink@gmail.com>
2022-06-29 12:30:33 +0000

    Fix release process to remove docker in docker
    Removes the docker inside docker to simplify and making publishing work.

--- script/release.sh
+++ script/release.sh
@@ -4,30 +4,6 @@ set -eu
 
 WORKDIR=$(pwd)
 
-
-export DEBIAN_FRONTEND=noninteractive
-
-echo "--- installing docker cli"
-apt-get update
-apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-apt-get update
-apt-get install -y docker-ce-cli 
-
-echo "Login to the docker..."
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-
 export GORELEASER_CURRENT_TAG=$(buildkite-agent meta-data get "release-version")
 
 cd $WORKDIR
