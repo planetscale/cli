a07db86fc43da84968a321791b1f18f4dd98ccf8
Nick Van Wiggeren <nick@planetscale.com>
2022-07-11 16:01:42 -0700

    add keyspace list command

--- internal/cmd/branch/branch.go
+++ internal/cmd/branch/branch.go
@@ -31,6 +31,7 @@ func BranchCmd(ch *cmdutil.Helper) *cobra.Command {
 	cmd.AddCommand(RefreshSchemaCmd(ch))
 	cmd.AddCommand(PromoteCmd(ch))
 	cmd.AddCommand(VSchemaCmd(ch))
+	cmd.AddCommand(KeyspaceCmd(ch))
 
 	return cmd
 }
@@ -73,6 +74,47 @@ func toDatabaseBranches(branches []*ps.DatabaseBranch) []*DatabaseBranch {
 
 	for _, db := range branches {
 		bs = append(bs, ToDatabaseBranch(db))
+	}
+
+	return bs
+}
+
+type DatabaseBranchKeyspace struct {
+	Name      string `header:"name" json:"name"`
+	Shards    int    `header:"shards" json:"shards"`
+	Sharded   bool   `header:"sharded" json:"sharded"`
+	CreatedAt int64  `header:"created_at,timestamp(ms|utc|human)" json:"created_at"`
+	UpdatedAt int64  `header:"updated_at,timestamp(ms|utc|human)" json:"updated_at"`
+
+	orig *ps.Keyspace
+}
+
+func (d *DatabaseBranchKeyspace) MarshalJSON() ([]byte, error) {
+	return json.MarshalIndent(d.orig, "", "  ")
+}
+
+func (d *DatabaseBranchKeyspace) MarshalCSVValue() interface{} {
+	return []*DatabaseBranchKeyspace{d}
+}
+
+// ToDatabaseBranch returns a struct that prints out the various fields of a
+// database model.
+func ToDatabaseBranchKeyspace(ks *ps.Keyspace) *DatabaseBranchKeyspace {
+	return &DatabaseBranchKeyspace{
+		Name:      ks.Name,
+		Shards:    ks.Shards,
+		Sharded:   ks.Sharded,
+		CreatedAt: ks.CreatedAt.UTC().UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond)),
+		UpdatedAt: ks.UpdatedAt.UTC().UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond)),
+		orig:      ks,
+	}
+}
+
+func toDatabaseBranchkeyspaces(keyspaces []*ps.Keyspace) []*DatabaseBranchKeyspace {
+	bs := make([]*DatabaseBranchKeyspace, 0, len(keyspaces))
+
+	for _, ks := range keyspaces {
+		bs = append(bs, ToDatabaseBranchKeyspace(ks))
 	}
 
 	return bs
