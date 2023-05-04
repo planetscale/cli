b1890e0ecdd96d07398a6e4e2b4fbe42335d008e
Phani Raj <phani@planetscale.com>
2023-05-04 11:28:01 -0500

    CR feedback

--- internal/cmd/database/create.go
+++ internal/cmd/database/create.go
@@ -55,7 +55,7 @@ func CreateCmd(ch *cmdutil.Helper) *cobra.Command {
 
 			if org.RemainingFreeDatabases == 0 {
 				ch.Printer.Printf("Organization [%s] does not have any free databases remaining\n", org.Name)
-				ch.Printer.Printf("If you choose to continue, this  database will be created on Scaler plan. The monthly cost is %v.\n", printer.BoldYellow("$29"))
+				ch.Printer.Printf("If you choose to continue, this database will be created on the Scaler plan. The monthly cost is %v.\n", printer.BoldYellow("$29"))
 				confirmationName := "Y"
 				confirmError := ch.Printer.ConfirmCommand(confirmationName, "create", "create this branch")
 				if confirmError != nil {
