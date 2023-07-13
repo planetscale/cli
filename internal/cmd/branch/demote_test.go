38006333f1028aee1f1c9212a8fe3a4f50a9b7ab
Iheanyi Ekechukwu <iheanyi@planetscale.com>
2023-07-13 16:44:27 -0500

    Update tests

--- internal/cmd/branch/demote_test.go
+++ internal/cmd/branch/demote_test.go
@@ -39,65 +39,6 @@ func TestBranch_DemoteCmd(t *testing.T) {
 
 			return res, nil
 		},
-		GetFn: func(ctx context.Context, req *ps.GetDatabaseBranchRequest) (*ps.DatabaseBranch, error) {
-			c.Assert(req.Branch, qt.Equals, branch)
-			c.Assert(req.Database, qt.Equals, db)
-			c.Assert(req.Organization, qt.Equals, org)
-
-			return res, nil
-		},
-	}
-
-	ch := &cmdutil.Helper{
-		Printer: p,
-		Config: &config.Config{
-			Organization: org,
-		},
-		Client: func() (*ps.Client, error) {
-			return &ps.Client{
-				DatabaseBranches: svc,
-			}, nil
-		},
-	}
-
-	cmd := DemoteCmd(ch)
-	cmd.SetArgs([]string{db, branch})
-	err := cmd.Execute()
-
-	c.Assert(err, qt.IsNil)
-	c.Assert(svc.DemoteFnInvoked, qt.IsTrue)
-	c.Assert(svc.GetFnInvoked, qt.IsTrue)
-	c.Assert(buf.String(), qt.JSONEquals, res)
-}
-
-func TestBranch_DemoteCmdWithDemotionRequest(t *testing.T) {
-	c := qt.New(t)
-
-	var buf bytes.Buffer
-	format := printer.JSON
-	p := printer.NewPrinter(&format)
-	p.SetResourceOutput(&buf)
-
-	org := "planetscale"
-	db := "planetscale"
-	branch := "development"
-
-	res := &ps.BranchDemotionRequest{
-		ID:    "test",
-		State: "pending",
-		Actor: &ps.Actor{
-			Name: "Test User",
-		},
-	}
-
-	svc := &mock.DatabaseBranchesService{
-		DemoteFn: func(ctx context.Context, req *ps.DemoteRequest) (*ps.BranchDemotionRequest, error) {
-			c.Assert(req.Branch, qt.Equals, branch)
-			c.Assert(req.Database, qt.Equals, db)
-			c.Assert(req.Organization, qt.Equals, org)
-
-			return res, nil
-		},
 	}
 
 	ch := &cmdutil.Helper{
