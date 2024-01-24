e78a91a88bc47d294ddae6daf3a88f1a2f62062e
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2024-01-24 08:09:09 -0500

    Remove plan confirmation on database create
    This is no longer needed because the API does not auto create a scaler
    DB anymore without the plan explicitly being passed.
    
    Now if a user does not pass a plan, it will default to free. If they are out of free DBs, they'll get a message back.

--- internal/cmd/database/create.go
+++ internal/cmd/database/create.go
@@ -71,16 +71,6 @@ func CreateCmd(ch *cmdutil.Helper) *cobra.Command {
 				return err
 			}
 
-			if org.RemainingFreeDatabases == 0 && !force {
-				ch.Printer.Printf("Organization [%s] does not have any free databases remaining\n", org.Name)
-				ch.Printer.Printf("If you choose to continue, this database will be created on the Scaler plan. The monthly cost is %v.\n", printer.BoldYellow("$29"))
-				confirmationName := "Y"
-				confirmError := ch.Printer.ConfirmCommand(confirmationName, "create", "create this database")
-				if confirmError != nil {
-					return confirmError
-				}
-			}
-
 			end := ch.Printer.PrintProgress("Creating database...")
 			defer end()
 			database, err := client.Databases.Create(ctx, createReq)
