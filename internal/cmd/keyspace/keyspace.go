207b68ae73808766cd95bb7984013c5977257d94
Iheanyi Ekechukwu <iheanyi@planetscale.com>
2024-10-02 15:26:17 -0500

    Add pending resize to keyspace

--- internal/cmd/keyspace/keyspace.go
+++ internal/cmd/keyspace/keyspace.go
@@ -29,12 +29,14 @@ func KeyspaceCmd(ch *cmdutil.Helper) *cobra.Command {
 }
 
 type BranchKeyspace struct {
-	ID          string `json:"id"`
-	Name        string `header:"name" json:"name"`
-	Shards      int    `header:"shards" json:"shards"`
-	Sharded     bool   `header:"sharded" json:"sharded"`
-	Replicas    uint64 `header:"replicas" json:"replicas"`
-	Resizing    bool   `header:"resizing" json:"resizing"`
+	ID            string `json:"id"`
+	Name          string `header:"name" json:"name"`
+	Shards        int    `header:"shards" json:"shards"`
+	Sharded       bool   `header:"sharded" json:"sharded"`
+	Replicas      uint64 `header:"replicas" json:"replicas"`
+	Resizing      bool   `header:"resizing" json:"resizing"`
+	PendingResize bool   `header:"pending_resize" json:"resize_pending"`
+
 	ClusterSize string `header:"cluster_size" json:"cluster_rate_name"`
 	CreatedAt   int64  `header:"created_at,timestamp(ms|utc|human)" json:"created_at"`
 	UpdatedAt   int64  `header:"updated_at,timestamp(ms|utc|human)" json:"updated_at"`
