df983ac47dbe600105103f45ef9b371e174c53de
Iheanyi Ekechukwu <iheanyi@planetscale.com>
2024-10-03 13:10:19 -0500

    Add resize status command

--- internal/cmd/keyspace/resize.go
+++ internal/cmd/keyspace/resize.go
@@ -88,6 +88,50 @@ func ResizeCmd(ch *cmdutil.Helper) *cobra.Command {
 	cmd.Flags().IntVar(&flags.additionalReplicas, "additional-replicas", 0, "Number of additional additionalReplicas to add to the keyspace")
 	cmd.Flags().StringVar(&flags.clusterSize, "cluster-size", "", "The cluster size to use for the keyspace")
 
+	cmd.AddCommand(StatusCmd(ch))
+
+	return cmd
+}
+
+func StatusCmd(ch *cmdutil.Helper) *cobra.Command {
+	statusReq := &ps.KeyspaceResizeStatusRequest{}
+
+	cmd := &cobra.Command{
+		Use:   "resize status <database> <branch> <keyspace>",
+		Short: "Gets the status of the last resize operation on a keyspace",
+		Args:  cmdutil.RequiredArgs("database", "branch", "keyspace"),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := cmd.Context()
+			database, branch, keyspace := args[0], args[1], args[2]
+
+			statusReq.Organization = ch.Config.Organization
+			statusReq.Database = database
+			statusReq.Branch = branch
+			statusReq.Keyspace = keyspace
+
+			client, err := ch.Client()
+			if err != nil {
+				return err
+			}
+
+			end := ch.Printer.PrintProgress(fmt.Sprintf("Saving changes to keyspace %s in %s/%s", keyspace, database, branch))
+			defer end()
+
+			krr, err := client.Keyspaces.ResizeStatus(ctx, statusReq)
+			if err != nil {
+				switch cmdutil.ErrCode(err) {
+				case ps.ErrNotFound:
+					return fmt.Errorf("database %s or branch %s or keyspace %s does not exist in organization %s", printer.BoldBlue(database), printer.BoldBlue(branch), printer.BoldBlue(keyspace), printer.BoldBlue(ch.Config.Organization))
+				default:
+					return cmdutil.HandleError(err)
+				}
+			}
+			end()
+
+			return ch.Printer.PrintResource(toKeyspaceResizeRequest(krr))
+		},
+	}
+
 	return cmd
 }
 
