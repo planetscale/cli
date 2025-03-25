3ecd037583e3ed3b769ccf67e20aea68531f8f56
Iheanyi Ekechukwu <iheanyi@planetscale.com>
2025-03-25 12:20:55 -0500

    Implement workflow complete command

---
+++ internal/cmd/workflow/complete.go
@@ -0,0 +1,65 @@
+package workflow
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/planetscale/cli/internal/cmdutil"
+	"github.com/planetscale/cli/internal/printer"
+	ps "github.com/planetscale/planetscale-go/planetscale"
+	"github.com/spf13/cobra"
+)
+
+func CompleteCmd(ch *cmdutil.Helper) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "complete <database> <number>",
+		Short: "Mark a workflow as complete",
+		Long:  `Mark a workflow as complete. This is done after the cutover is confirmed and the application is working as expected.`,
+		Args:  cmdutil.RequiredArgs("database", "number"),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := cmd.Context()
+			db, num := args[0], args[1]
+
+			client, err := ch.Client()
+			if err != nil {
+				return err
+			}
+
+			var number uint64
+			number, err = strconv.ParseUint(num, 10, 64)
+			if err != nil {
+				return err
+			}
+
+			end := ch.Printer.PrintProgress(fmt.Sprintf("Marking workflow %s in database %s as complete...", printer.BoldBlue(number), printer.BoldBlue(db)))
+			defer end()
+
+			workflow, err := client.Workflows.Complete(ctx, &ps.CompleteWorkflowRequest{
+				Organization:   ch.Config.Organization,
+				Database:       db,
+				WorkflowNumber: number,
+			})
+			if err != nil {
+				switch cmdutil.ErrCode(err) {
+				case ps.ErrNotFound:
+					return fmt.Errorf("database %s or workflow %s does not exist in organization %s",
+						printer.BoldBlue(db), printer.BoldBlue(number), printer.BoldBlue(ch.Config.Organization))
+				default:
+					return cmdutil.HandleError(err)
+				}
+			}
+
+			if ch.Printer.Format() == printer.Human {
+				ch.Printer.Printf("Workflow %s in database %s has been marked as complete.\n",
+					printer.BoldBlue(workflow.Name),
+					printer.BoldBlue(db),
+				)
+				return nil
+			}
+
+			return ch.Printer.PrintResource(toWorkflow(workflow))
+		},
+	}
+
+	return cmd
+}
