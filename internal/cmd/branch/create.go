4a7fff8375ddcf73b949f45e1de67f9e879945c8
Iheanyi Ekechukwu <iheanyi@planetscale.com>
2025-07-08 16:54:52 -0500

    Add ability to create Postgres branches

--- internal/cmd/branch/create.go
+++ internal/cmd/branch/create.go
@@ -13,11 +13,13 @@ import (
 )
 
 func CreateCmd(ch *cmdutil.Helper) *cobra.Command {
-	createReq := &ps.CreateDatabaseBranchRequest{}
-
 	var flags struct {
 		wait          bool
 		dataBranching bool
+		region        string
+		parentBranch  string
+		clusterSize   string
+		backupID      string
 	}
 
 	cmd := &cobra.Command{
@@ -66,14 +68,6 @@ func CreateCmd(ch *cmdutil.Helper) *cobra.Command {
 			source := args[0]
 			branch := args[1]
 
-			if flags.dataBranching {
-				createReq.SeedData = "last_successful_backup"
-			}
-
-			createReq.Database = source
-			createReq.Name = branch
-			createReq.Organization = ch.Config.Organization
-
 			client, err := ch.Client()
 			if err != nil {
 				return err
@@ -81,56 +75,141 @@ func CreateCmd(ch *cmdutil.Helper) *cobra.Command {
 
 			end := ch.Printer.PrintProgress(fmt.Sprintf("Creating branch from %s...", printer.BoldBlue(source)))
 			defer end()
-			dbBranch, err := client.DatabaseBranches.Create(cmd.Context(), createReq)
+
+			db, err := client.Databases.Get(ctx, &ps.GetDatabaseRequest{
+				Organization: ch.Config.Organization,
+				Database:     source,
+			})
 			if err != nil {
 				switch cmdutil.ErrCode(err) {
 				case ps.ErrNotFound:
-					if createReq.ParentBranch != "" {
-						return cmdutil.HandleNotFoundWithServiceTokenCheck(
-							ctx, cmd, ch.Config, ch.Client, err, "create_branch",
-							"source branch %s or database %s does not exist (organization: %s)",
-							printer.BoldBlue(createReq.ParentBranch), printer.BoldBlue(source), printer.BoldBlue(ch.Config.Organization))
-					} else {
-						return cmdutil.HandleNotFoundWithServiceTokenCheck(
-							ctx, cmd, ch.Config, ch.Client, err, "create_branch",
-							"source database %s does not exist in organization %s",
-							printer.BoldBlue(source), printer.BoldBlue(ch.Config.Organization))
-					}
+					return cmdutil.HandleNotFoundWithServiceTokenCheck(
+						ctx, cmd, ch.Config, ch.Client, err, "read_branch",
+						"branch %s does not exist in database %s (organization: %s)",
+						printer.BoldBlue(branch), printer.BoldBlue(source), printer.BoldBlue(ch.Config.Organization))
 				default:
 					return cmdutil.HandleError(err)
 				}
 			}
 
-			end()
-
-			// wait and check until the DB is ready
-			if flags.wait {
-				end := ch.Printer.PrintProgress(fmt.Sprintf("Waiting until branch %s is ready...", printer.BoldBlue(branch)))
-				defer end()
-				getReq := &ps.GetDatabaseBranchRequest{
+			if db.Kind == "mysql" {
+				createReq := &ps.CreateDatabaseBranchRequest{
 					Organization: ch.Config.Organization,
 					Database:     source,
-					Branch:       branch,
+					Name:         branch,
+					Region:       flags.region,
+					ClusterSize:  flags.clusterSize,
+					ParentBranch: flags.parentBranch,
+					BackupID:     flags.backupID,
 				}
-				if err := waitUntilReady(ctx, client, ch.Printer, ch.Debug(), getReq); err != nil {
-					return err
+
+				if flags.dataBranching {
+					createReq.SeedData = "last_successful_backup"
 				}
+
+				dbBranch, err := client.DatabaseBranches.Create(cmd.Context(), createReq)
+				if err != nil {
+					switch cmdutil.ErrCode(err) {
+					case ps.ErrNotFound:
+						if createReq.ParentBranch != "" {
+							return cmdutil.HandleNotFoundWithServiceTokenCheck(
+								ctx, cmd, ch.Config, ch.Client, err, "create_branch",
+								"source branch %s or database %s does not exist (organization: %s)",
+								printer.BoldBlue(createReq.ParentBranch), printer.BoldBlue(source), printer.BoldBlue(ch.Config.Organization))
+						} else {
+							return cmdutil.HandleNotFoundWithServiceTokenCheck(
+								ctx, cmd, ch.Config, ch.Client, err, "create_branch",
+								"source database %s does not exist in organization %s",
+								printer.BoldBlue(source), printer.BoldBlue(ch.Config.Organization))
+						}
+					default:
+						return cmdutil.HandleError(err)
+					}
+				}
+
 				end()
-			}
 
-			if ch.Printer.Format() == printer.Human {
-				ch.Printer.Printf("Branch %s was successfully created.\n\nView this branch in the browser: %s\n", printer.BoldBlue(dbBranch.Name), printer.BoldBlue(dbBranch.HtmlURL))
-				return nil
-			}
+				// wait and check until the DB is ready
+				if flags.wait {
+					end := ch.Printer.PrintProgress(fmt.Sprintf("Waiting until branch %s is ready...", printer.BoldBlue(branch)))
+					defer end()
+					getReq := &ps.GetDatabaseBranchRequest{
+						Organization: ch.Config.Organization,
+						Database:     source,
+						Branch:       branch,
+					}
+					if err := waitUntilReady(ctx, client, ch.Printer, ch.Debug(), getReq); err != nil {
+						return err
+					}
+					end()
+				}
+				if ch.Printer.Format() == printer.Human {
+					ch.Printer.Printf("Branch %s was successfully created.\n\nView this branch in the browser: %s\n", printer.BoldBlue(dbBranch.Name), printer.BoldBlue(dbBranch.HtmlURL))
+					return nil
+				}
 
-			return ch.Printer.PrintResource(ToDatabaseBranch(dbBranch))
+				return ch.Printer.PrintResource(ToDatabaseBranch(dbBranch))
+			} else {
+				createReq := &ps.CreatePostgresBranchRequest{
+					Organization: ch.Config.Organization,
+					Database:     source,
+					Name:         branch,
+					Region:       flags.region,
+					ClusterName:  flags.clusterSize,
+					ParentBranch: flags.parentBranch,
+					BackupID:     flags.backupID,
+				}
+
+				dbBranch, err := client.PostgresBranches.Create(cmd.Context(), createReq)
+				if err != nil {
+					switch cmdutil.ErrCode(err) {
+					case ps.ErrNotFound:
+						if createReq.ParentBranch != "" {
+							return cmdutil.HandleNotFoundWithServiceTokenCheck(
+								ctx, cmd, ch.Config, ch.Client, err, "create_branch",
+								"source branch %s or database %s does not exist (organization: %s)",
+								printer.BoldBlue(createReq.ParentBranch), printer.BoldBlue(source), printer.BoldBlue(ch.Config.Organization))
+						} else {
+							return cmdutil.HandleNotFoundWithServiceTokenCheck(
+								ctx, cmd, ch.Config, ch.Client, err, "create_branch",
+								"source database %s does not exist in organization %s",
+								printer.BoldBlue(source), printer.BoldBlue(ch.Config.Organization))
+						}
+					default:
+						return cmdutil.HandleError(err)
+					}
+				}
+
+				end()
+
+				// wait and check until the DB is ready
+				if flags.wait {
+					end := ch.Printer.PrintProgress(fmt.Sprintf("Waiting until branch %s is ready...", printer.BoldBlue(branch)))
+					defer end()
+					getReq := &ps.GetPostgresBranchRequest{
+						Organization: ch.Config.Organization,
+						Database:     source,
+						Branch:       branch,
+					}
+					if err := waitUntilPostgresReady(ctx, client, ch.Printer, ch.Debug(), getReq); err != nil {
+						return err
+					}
+					end()
+				}
+				if ch.Printer.Format() == printer.Human {
+					ch.Printer.Printf("Branch %s was successfully created in %s.\n", printer.BoldBlue(dbBranch.Name), printer.BoldBlue(source))
+					return nil
+				}
+
+				return ch.Printer.PrintResource(ToPostgresBranch(dbBranch))
+			}
 		},
 	}
 
-	cmd.Flags().StringVar(&createReq.ParentBranch, "from", "", "Parent branch to create the new branch from. Cannot be used with --restore")
-	cmd.Flags().StringVar(&createReq.Region, "region", "", "Region for the branch to be created in.")
-	cmd.Flags().StringVar(&createReq.BackupID, "restore", "", "ID of Backup to restore into branch.")
-	cmd.Flags().StringVar(&createReq.ClusterSize, "cluster-size", "PS-10", "Cluster size for branches being created from a backup or seeded with data. Use 'pscale size cluster list' to see the valid sizes.")
+	cmd.Flags().StringVar(&flags.parentBranch, "from", "", "Parent branch to create the new branch from. Cannot be used with --restore")
+	cmd.Flags().StringVar(&flags.region, "region", "", "Region for the branch to be created in.")
+	cmd.Flags().StringVar(&flags.backupID, "restore", "", "ID of Backup to restore into branch.")
+	cmd.Flags().StringVar(&flags.clusterSize, "cluster-size", "PS-10", "Cluster size for branches being created from a backup or seeded with data. Use 'pscale size cluster list' to see the valid sizes.")
 	cmd.Flags().BoolVar(&flags.dataBranching, "seed-data", false, "Add seed data using the Data Branching™ feature. This branch will be created with the same resources as the base branch.")
 	cmd.Flags().BoolVar(&flags.wait, "wait", false, "Wait until the branch is ready")
 	cmd.MarkFlagsMutuallyExclusive("from", "restore")
@@ -160,6 +239,32 @@ func waitUntilReady(ctx context.Context, client *ps.Client, printer *printer.Pri
 			return errors.New("branch creation timed out")
 		case <-ticker.C:
 			resp, err := client.DatabaseBranches.Get(ctx, getReq)
+			if err != nil {
+				if debug {
+					printer.Printf("fetching database branch %s/%s failed: %s", getReq.Database, getReq.Branch, err)
+				}
+				continue
+			}
+
+			if resp.Ready {
+				return nil
+			}
+		}
+	}
+}
+
+func waitUntilPostgresReady(ctx context.Context, client *ps.Client, printer *printer.Printer, debug bool, getReq *ps.GetPostgresBranchRequest) error {
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Minute)
+	defer cancel()
+
+	ticker := time.NewTicker(time.Second)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return errors.New("branch creation timed out")
+		case <-ticker.C:
+			resp, err := client.PostgresBranches.Get(ctx, getReq)
 			if err != nil {
 				if debug {
 					printer.Printf("fetching database branch %s/%s failed: %s", getReq.Database, getReq.Branch, err)
