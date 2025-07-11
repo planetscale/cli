849deac140348e86786e3480c2b73c9c7f418580
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2025-07-11 14:21:24 -0500

    Rename `kind` flag for database creation (#1086)
    * Switch flag name to engine instead of kind
    
    * Use string enum for the database engine
    
    * Simplify flags
    
    * Update planetscale-go

--- internal/cmd/database/create.go
+++ internal/cmd/database/create.go
@@ -15,6 +15,11 @@ import (
 func CreateCmd(ch *cmdutil.Helper) *cobra.Command {
 	createReq := &ps.CreateDatabaseRequest{}
 
+	var flags struct {
+		clusterSize string
+		engine      string
+	}
+
 	cmd := &cobra.Command{
 		Use:   "create <database>",
 		Short: "Create a database instance",
@@ -25,12 +30,12 @@ func CreateCmd(ch *cmdutil.Helper) *cobra.Command {
 			createReq.Organization = ch.Config.Organization
 			createReq.Name = args[0]
 
-			clusterSize, err := cmd.Flags().GetString("cluster-size")
+			engine, err := parseDatabaseEngine(flags.engine)
 			if err != nil {
 				return err
 			}
 
-			createReq.ClusterSize = clusterSize
+			createReq.Kind = engine
 
 			client, err := ch.Client()
 			if err != nil {
@@ -62,10 +67,9 @@ func CreateCmd(ch *cmdutil.Helper) *cobra.Command {
 
 	cmd.Flags().StringVar(&createReq.Region, "region", "", "region for the database")
 
-	cmd.Flags().String("cluster-size", "", "cluster size for Scaler Pro databases. Use `pscale size cluster list` to see the valid sizes.")
-
-	cmd.Flags().StringVar(&createReq.Kind, "kind", "mysql", "Kind of database to create. Supported values: mysql, postgresql. Defaults to mysql.")
-	cmd.RegisterFlagCompletionFunc("kind", func(cmd *cobra.Command, args []string, toComplete string) ([]cobra.Completion, cobra.ShellCompDirective) {
+	cmd.Flags().StringVar(&createReq.ClusterSize, "cluster-size", "", "cluster size for Scaler Pro databases. Use `pscale size cluster list` to see the valid sizes.")
+	cmd.Flags().StringVar(&flags.engine, "engine", string(ps.DatabaseEngineMySQL), "The database engine for the database. Supported values: mysql, postgresql. Defaults to mysql.")
+	cmd.RegisterFlagCompletionFunc("engine", func(cmd *cobra.Command, args []string, toComplete string) ([]cobra.Completion, cobra.ShellCompDirective) {
 		return []cobra.Completion{
 			cobra.CompletionWithDesc("mysql", "A Vitess database"),
 			cobra.CompletionWithDesc("postgresql", "The fastest cloud Postgres"),
@@ -81,4 +85,15 @@ func CreateCmd(ch *cmdutil.Helper) *cobra.Command {
 	})
 
 	return cmd
+}
+
+func parseDatabaseEngine(engine string) (ps.DatabaseEngine, error) {
+	switch engine {
+	case "mysql":
+		return ps.DatabaseEngineMySQL, nil
+	case "postgresql", "postgres":
+		return ps.DatabaseEnginePostgres, nil
+	default:
+		return ps.DatabaseEngineMySQL, fmt.Errorf("invalid database engine %q, supported values: mysql, postgresql", engine)
+	}
 }
--- internal/cmd/database/create_test.go
+++ internal/cmd/database/create_test.go
@@ -65,3 +65,56 @@ func TestDatabase_CreateCmd(t *testing.T) {
 	c.Assert(svc.CreateFnInvoked, qt.IsTrue)
 	c.Assert(buf.String(), qt.JSONEquals, res)
 }
+
+func TestDatabase_CreateCmdPostgres(t *testing.T) {
+	c := qt.New(t)
+
+	var buf bytes.Buffer
+	format := printer.JSON
+	p := printer.NewPrinter(&format)
+	p.SetResourceOutput(&buf)
+
+	org := "planetscale"
+	db := "planetscale"
+
+	res := &ps.Database{Name: "foo"}
+
+	svc := &mock.DatabaseService{
+		CreateFn: func(ctx context.Context, req *ps.CreateDatabaseRequest) (*ps.Database, error) {
+			c.Assert(req.Organization, qt.Equals, org)
+			c.Assert(req.Name, qt.Equals, db)
+			c.Assert(req.Region, qt.Equals, "us-east")
+			c.Assert(req.Kind, qt.Equals, ps.DatabaseEnginePostgres)
+
+			return res, nil
+		},
+	}
+
+	ch := &cmdutil.Helper{
+		Printer: p,
+		Config: &config.Config{
+			Organization: org,
+		},
+		Client: func() (*ps.Client, error) {
+			return &ps.Client{
+				Databases: svc,
+				Organizations: &mock.OrganizationsService{
+					GetFn: func(ctx context.Context, request *ps.GetOrganizationRequest) (*ps.Organization, error) {
+						return &ps.Organization{
+							RemainingFreeDatabases: 1,
+							Name:                   request.Organization,
+						}, nil
+					},
+				},
+			}, nil
+		},
+	}
+
+	cmd := CreateCmd(ch)
+	cmd.SetArgs([]string{db, "--region", "us-east", "--engine", "postgresql"})
+	err := cmd.Execute()
+
+	c.Assert(err, qt.IsNil)
+	c.Assert(svc.CreateFnInvoked, qt.IsTrue)
+	c.Assert(buf.String(), qt.JSONEquals, res)
+}
