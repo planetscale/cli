13f412935fa75e8f72bca19fc672ddef45d5cdd7
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2025-12-03 11:03:37 -0500

    Add `current` to `org list` output
    Closes: https://github.com/planetscale/cli/issues/1145
    
    Allows the user to see which org is currently selected by adding
    `current` to the output.
    
    Human format:
    ```
      NAME (25)               CREATED AT     UPDATED AT     CURRENT
     ----------------------- -------------- -------------- ---------
      * mike                  4 years ago    2 years ago    Yes
      mikes-cool-org          4 years ago    2 years ago    No
    ```
    
    JSON format:
    ```
      {
        "name": "mike",
        "created_at": 1610391461709,
        "updated_at": 1710356459058,
        "current": true
      },
      {
        "name": "mikes-cool-org",
        "created_at": 1612538280956,
        "updated_at": 1690478112026,
        "current": false
      },
    ```

--- internal/cmd/org/org.go
+++ internal/cmd/org/org.go
@@ -2,6 +2,7 @@ package org
 
 import (
 	"github.com/planetscale/cli/internal/cmdutil"
+	"github.com/planetscale/cli/internal/config"
 	"github.com/planetscale/cli/internal/printer"
 	ps "github.com/planetscale/planetscale-go/planetscale"
 	"github.com/spf13/cobra"
@@ -26,23 +27,56 @@ type organization struct {
 	Name      string `header:"name" json:"name"`
 	CreatedAt int64  `header:"created_at,timestamp(ms|utc|human)" json:"created_at"`
 	UpdatedAt int64  `header:"updated_at,timestamp(ms|utc|human)" json:"updated_at"`
+	Current   bool   `header:"current" json:"current"`
 }
 
 // toOrgs returns a slice of printable orgs.
-func toOrgs(organizations []*ps.Organization) []*organization {
+func toOrgs(organizations []*ps.Organization, currentOrgName string, format printer.Format) []*organization {
 	orgs := make([]*organization, 0, len(organizations))
 
 	for _, org := range organizations {
-		orgs = append(orgs, toOrg(org))
+		orgs = append(orgs, toOrg(org, currentOrgName, format))
 	}
 
 	return orgs
 }
 
-func toOrg(org *ps.Organization) *organization {
+func toOrg(org *ps.Organization, currentOrgName string, format printer.Format) *organization {
+	isCurrent := org.Name == currentOrgName
+	name := org.Name
+
+	if isCurrent && format == printer.Human {
+		name = "* " + org.Name
+	}
+
 	return &organization{
-		Name:      org.Name,
+		Name:      name,
 		CreatedAt: printer.GetMilliseconds(org.CreatedAt),
 		UpdatedAt: printer.GetMilliseconds(org.UpdatedAt),
+		Current:   isCurrent,
 	}
+}
+
+func getCurrentOrganization(ch *cmdutil.Helper) string {
+	if ch.Config.Organization != "" {
+		return ch.Config.Organization
+	}
+
+	configPath, err := config.ProjectConfigPath()
+	if err == nil {
+		cfg, err := ch.ConfigFS.NewFileConfig(configPath)
+		if err == nil && cfg.Organization != "" {
+			return cfg.Organization
+		}
+	}
+
+	configPath, err = config.DefaultConfigPath()
+	if err == nil {
+		cfg, err := ch.ConfigFS.NewFileConfig(configPath)
+		if err == nil && cfg.Organization != "" {
+			return cfg.Organization
+		}
+	}
+
+	return ""
 }
