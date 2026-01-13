d2c135c9d417d11babf1a469c23febdd09684154
Simeon Griggs <simeon@hey.com>
2026-01-13 15:44:19 +0000

    fix: improve error message when role command returns 404

--- internal/cmd/role/create.go
+++ internal/cmd/role/create.go
@@ -54,8 +54,8 @@ func CreateCmd(ch *cmdutil.Helper) *cobra.Command {
 			if err != nil {
 				switch cmdutil.ErrCode(err) {
 				case ps.ErrNotFound:
-					return fmt.Errorf("branch %s does not exist in database %s (organization: %s)",
-						printer.BoldBlue(branch), printer.BoldBlue(database), printer.BoldBlue(ch.Config.Organization))
+					return fmt.Errorf("database %s or branch %s does not exist in organization %s",
+						printer.BoldBlue(database), printer.BoldBlue(branch), printer.BoldBlue(ch.Config.Organization))
 				default:
 					return cmdutil.HandleError(err)
 				}
