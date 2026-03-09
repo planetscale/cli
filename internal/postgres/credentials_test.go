87b6919179149f9233c50e5ea41d28d8288523fd
Elom Gomez <elomgomez@gmail.com>
2026-03-09 13:49:38 -0500

    Clean up unneeded tests

--- internal/postgres/credentials_test.go
+++ internal/postgres/credentials_test.go
@@ -5,26 +5,6 @@ import (
 	"testing"
 )
 
-func TestImportInfo(t *testing.T) {
-	// Basic structure test - ensure ImportInfo struct is usable
-	info := &ImportInfo{
-		SourceConnStr:   "postgresql://localhost:5432/db",
-		RoleID:          "role123",
-		RoleUsername:    "user",
-		RolePassword:    "pass",
-		RoleHost:        "host",
-		PublicationName: "pub",
-		DBName:          "postgres",
-	}
-
-	if info.SourceConnStr == "" {
-		t.Error("ImportInfo should have SourceConnStr")
-	}
-	if info.RoleID == "" {
-		t.Error("ImportInfo should have RoleID")
-	}
-}
-
 func TestStoreAndRetrieveImportCredentials(t *testing.T) {
 	// Enable test mode to use file backend
 	os.Setenv("PSCALE_TEST_MODE", "1")
