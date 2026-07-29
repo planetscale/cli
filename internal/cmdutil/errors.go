ef49f7cd3f0a1e94d35e3d5d94e9060c6d1b6225
bwarminski <bwarminski@planetscale.com>
2026-07-29 16:05:03 -0400

    Handle wrapped API errors

--- internal/cmdutil/errors.go
+++ internal/cmdutil/errors.go
@@ -56,8 +56,8 @@ func HandleError(err error) error {
 		return err
 	}
 
-	perr, ok := err.(*planetscale.Error)
-	if !ok {
+	var perr *planetscale.Error
+	if !errors.As(err, &perr) {
 		return err
 	}
 
